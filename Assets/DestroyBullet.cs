5ce715846bab9acf05eedbe69d3a6f5919977796
hanbaomo <863005737wei@gmail.com>
2023-05-08 14:26:00 +1000

    Added test script for enemy(perfabs A*), now the enemy is set as long-range enemy. 1. Keep distance from the  Player. 2. Shoot bullets, bullets will destroy and take damage if hits the player. 3. Debug fix, can face to the player in each frame now, "But" need to assign the player to the "Face Player" in the "TestScript".

--- Assets/DestroyBullet.cs
+++ Assets/DestroyBullet.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Player;
 
 public class DestroyBullet : MonoBehaviour
 {
+    public int damage = 5;
     // Start is called before the first frame update
     
 
@@ -11,6 +13,9 @@ public class DestroyBullet : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            PlayerCharacter PlayerCharacter = other.gameObject.GetComponent<PlayerCharacter>();
+            PlayerCharacter.TakeDamage(damage);
+            Debug.Log("Bullet hits!");
             Destroy(gameObject);
         }
     }
