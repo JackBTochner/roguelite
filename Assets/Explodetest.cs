8cc40746eda0e8a70cb2e88070e5526caf9a5551
hanbaomo <863005737wei@gmail.com>
2023-05-23 21:24:40 +1000

    Update damage for explode enemy.

--- Assets/Explodetest.cs
+++ Assets/Explodetest.cs
@@ -8,7 +8,8 @@ public class Explodetest : MonoBehaviour
     public GameObject target; // The player.
     public GameObject explosionPrefab; // The Explosion animation.
     public float triggerDistance = 1.0f; // The distance.
-    public int damage = 50;
+    public int fulldamage = 50;
+    public int middamage = 25;
 
     void Update()
     {
@@ -28,10 +29,15 @@ public class Explodetest : MonoBehaviour
         Instantiate(explosionPrefab, transform.position, Quaternion.identity); // Do the explosion.
         Destroy(gameObject); // Delete the explosion enemy.
 
-        if (Vector3.Distance(transform.position, target.transform.position) < 5f)
+        if (Vector3.Distance(transform.position, target.transform.position) < 3f)
         {
             PlayerCharacter playerCharacter = target.GetComponent<PlayerCharacter>();
-            playerCharacter.TakeDamage(damage);
+            playerCharacter.TakeDamage(fulldamage);
+        }
+        else if (3f < Vector3.Distance(transform.position, target.transform.position) && Vector3.Distance(transform.position, target.transform.position) < 6f)
+        {
+            PlayerCharacter playerCharacter = target.GetComponent<PlayerCharacter>();
+            playerCharacter.TakeDamage(middamage);
         }
     }
 }
