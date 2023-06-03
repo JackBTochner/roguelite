eb72af09290004de102736441dd22ccf6e82f3e7
pmuenjohn <muenjohnt@gmail.com>
2023-06-04 05:39:09 +1000

    Add Button cancelled events.

--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -14,16 +14,21 @@ public class InputReader : DescriptionBaseSO, Controls.IPlayerActions
     public Vector2 MoveComposite;
 
     public Action OnJumpPerformed;
+    public Action OnJumpCancelled;
 
     public Action OnRestartPerformed;
 
     public Action OnDigPerformed;
+    public Action OnDigCancelled;
 
     public Action OnAttack1Performed;
+    public Action OnAttack1Cancelled;
 
     public Action OnAttack2Performed;
+    public Action OnAttack2Cancelled;
 
     public Action OnDashPerformed;
+    public Action OnDashCancelled;
 
     public Controls controls;
 
@@ -43,14 +48,16 @@ public class InputReader : DescriptionBaseSO, Controls.IPlayerActions
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (!context.performed) return;
-        OnJumpPerformed?.Invoke();
+        if (context.performed)
+            OnJumpPerformed?.Invoke();
+        if (context.canceled)
+            OnJumpCancelled?.Invoke();
     }
 
     public void OnRestart(InputAction.CallbackContext context)
     {
-        if (!context.performed) return;
-        OnRestartPerformed?.Invoke();
+        if (context.performed)
+            OnRestartPerformed?.Invoke();
     }
 
     public void OnLook(InputAction.CallbackContext context)
@@ -70,23 +77,31 @@ public class InputReader : DescriptionBaseSO, Controls.IPlayerActions
 
     public void OnDig(InputAction.CallbackContext context)
     { 
-        if (!context.performed) return;
-        OnDigPerformed?.Invoke();
+        if (context.performed)
+            OnDigPerformed?.Invoke();
+        if (context.canceled)
+            OnDigCancelled?.Invoke();
     }
     
     public void OnAttack1(InputAction.CallbackContext context)
     { 
-        if (!context.performed) return;
-        OnAttack1Performed?.Invoke();
+        if (context.performed)
+            OnAttack1Performed?.Invoke();
+        if (context.canceled)
+            OnAttack1Cancelled?.Invoke();
     }
      public void OnAttack2(InputAction.CallbackContext context)
     { 
-        if (!context.performed) return;
-        OnAttack2Performed?.Invoke();
+        if (context.performed)
+            OnAttack2Performed?.Invoke();
+        if (context.canceled)
+            OnAttack2Cancelled?.Invoke();
     }
     public void OnDash(InputAction.CallbackContext context)
     { 
-        if (!context.performed) return;
-        OnDashPerformed?.Invoke();
+        if (context.performed)
+            OnDashPerformed?.Invoke();
+        if (context.canceled)
+            OnDashCancelled?.Invoke();
     }
 }
