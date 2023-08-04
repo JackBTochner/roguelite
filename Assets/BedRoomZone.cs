941ad3a9d33e72db94f91720b8d1d5214bfc50be
Ricky_Huo <113026826+hanbaomo@users.noreply.github.com>
2023-08-04 17:36:52 +1000

    Storefront zone finish

--- Assets/BedRoomZone.cs
+++ Assets/BedRoomZone.cs
@@ -2,25 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Player;
-using DG.Tweening;
+using DG.Tweening; //Animation engine for Unity
 
 public class BedRoomZone : MonoBehaviour
 {
+    // When the player spawn it will assign it to the player, it can get the current player, transform and rotation.
     public TransformAnchor playerTransformAnchor = default;
+    // The object that we want to fall, so this is the bedroom zone, we want move the storefront here.
     public GameObject storefront;
+    // The speed time when falling.
     public float fallTime;
+    // The distance the storefront falls. "Test": have to test, if the storefront is at this point, our camera should not see storefront anymore.
     public float fallDistance;
+    // We need to know the storefront is moving up or down. up is true, down is false.
     private bool isGoingUp = false;
 
+    //When the object collider touches something.
     private void OnTriggerEnter(Collider other)
     {
+        // When the object collider touches the "Player".
         if (other.CompareTag("Player"))
         {
+            // Get the position of the store front.
             Vector3 storeroomposition = storefront.transform.position;
+            // When the storefront is 0 and the player is lower than the position.
+            // This will not be used because the store front will always be at the bottom, if the player is in the bedroom - 
+            // - and not touching the collider yet.
             if(storeroomposition.y == 0 && playerTransformAnchor.Value.position.x < -14.45f)
             {
+                //Storefront will be falling, so set to false.
                 isGoingUp = false;
                 Debug.Log("isGoingUp set to false");
+
                 Transform storetransform = storefront.transform;
                 storetransform.DOMove(storetransform.position - new Vector3(0, fallDistance, 0), fallTime).SetEase(Ease.OutQuint);
             }
@@ -35,13 +48,13 @@ public class BedRoomZone : MonoBehaviour
             else if (storeroomposition.y < 0 && storeroomposition.y > -100 && isGoingUp == false)
             {
                 isGoingUp = true;
-                DOTween.Kill(storefront.transform); // Interrupt any ongoing tween
+                DOTween.Kill(storefront.transform);
                 storefront.transform.DOMove(new Vector3(storefront.transform.position.x, 0, storefront.transform.position.z), fallTime).SetEase(Ease.OutQuint);
             }
             else if (storeroomposition.y < 0 && storeroomposition.y > -100 && isGoingUp == true)
             {
                 isGoingUp = false;
-                DOTween.Kill(storefront.transform); // Interrupt any ongoing tween
+                DOTween.Kill(storefront.transform);
                 storefront.transform.DOMove(new Vector3(storefront.transform.position.x, -100, storefront.transform.position.z), fallTime).SetEase(Ease.OutQuint);
             }
         }
@@ -64,13 +77,13 @@ public class BedRoomZone : MonoBehaviour
                     else if (storeroomposition.y >= -100 && playerTransformAnchor.Value.position.x < -14.45f && isGoingUp == true)
                     {
                         isGoingUp = false;
-                        DOTween.Kill(storefront.transform); // Interrupt any ongoing tween
+                        DOTween.Kill(storefront.transform);
                         storefront.transform.DOMove(new Vector3(storefront.transform.position.x, -100, storefront.transform.position.z), fallTime).SetEase(Ease.OutQuint);
                     }
                     else if (storeroomposition.y >= -100 && playerTransformAnchor.Value.position.x < -14.45f && isGoingUp == false)
                     {
                         isGoingUp = true;
-                        DOTween.Kill(storefront.transform); // Interrupt any ongoing tween
+                        DOTween.Kill(storefront.transform);
                         storefront.transform.DOMove(new Vector3(storefront.transform.position.x, 0, storefront.transform.position.z), fallTime).SetEase(Ease.OutQuint);
                     }
                 }
--- Assets/StoreRoomZone.cs
+++ Assets/StoreRoomZone.cs
@@ -6,35 +6,75 @@ using DG.Tweening;
 
 public class StoreRoomZone : MonoBehaviour
 {
+    public TransformAnchor playerTransformAnchor = default;
     public GameObject bedroom;
     public float fallTime;
     public float fallDistance;
+    private bool isGoingUp = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Vector3 bedroomposition = bedroom.transform.position;
-            if(bedroomposition.y == 0)
+            if (bedroomposition.y == 0 && playerTransformAnchor.Value.position.x > -11.40f)
             {
+                isGoingUp = false;
+                Debug.Log("isGoingUp set to false");
                 Transform storetransform = bedroom.transform;
                 storetransform.DOMove(storetransform.position - new Vector3(0, fallDistance, 0), fallTime).SetEase(Ease.OutQuint);
             }
 
-            if(bedroomposition.y == -100)
+            else if (bedroomposition.y == -100)
             {
+                isGoingUp = true;
+                Debug.Log("isGoingUp set to true");
                 Transform storetransform = bedroom.transform;
                 storetransform.DOMove(storetransform.position + new Vector3(0, fallDistance, 0), fallTime).SetEase(Ease.OutQuint);
             }
+            else if (bedroomposition.y < 0 && bedroomposition.y > -100 && isGoingUp == false)
+            {
+                isGoingUp = true;
+                DOTween.Kill(bedroom.transform);
+                bedroom.transform.DOMove(new Vector3(bedroom.transform.position.x, 0, bedroom.transform.position.z), fallTime).SetEase(Ease.OutQuint);
+            }
+            else if (bedroomposition.y < 0 && bedroomposition.y > -100 && isGoingUp == true)
+            {
+                isGoingUp = false;
+                DOTween.Kill(bedroom.transform);
+                bedroom.transform.DOMove(new Vector3(bedroom.transform.position.x, -100, bedroom.transform.position.z), fallTime).SetEase(Ease.OutQuint);
+            }
         }
     }
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        OnTriggerEnter(other);
-    //    }
-    //}
-
-
+    private void OnTriggerExit(Collider other)
+    {
+        if (playerTransformAnchor != null && playerTransformAnchor.Value != null)
+        {
+            if (playerTransformAnchor.Value.position.x > -11.40f)
+            {
+                if (other.CompareTag("Player"))
+                {
+                    Vector3 bedroomposition = bedroom.transform.position;
+                    if (bedroomposition.y == 0 && playerTransformAnchor.Value.position.x > -11.40f)
+                    {
+                        isGoingUp = false;
+                        Transform storetransform = bedroom.transform;
+                        storetransform.DOMove(storetransform.position - new Vector3(0, fallDistance, 0), fallTime).SetEase(Ease.OutQuint);
+                    }
+                    else if (bedroomposition.y >= -100 && playerTransformAnchor.Value.position.x > -11.40f && isGoingUp == true)
+                    {
+                        isGoingUp = false;
+                        DOTween.Kill(bedroom.transform);
+                        bedroom.transform.DOMove(new Vector3(bedroom.transform.position.x, -100, bedroom.transform.position.z), fallTime).SetEase(Ease.OutQuint);
+                    }
+                    else if (bedroomposition.y >= -100 && playerTransformAnchor.Value.position.x > -11.40f && isGoingUp == false)
+                    {
+                        isGoingUp = true;
+                        DOTween.Kill(bedroom.transform);
+                        bedroom.transform.DOMove(new Vector3(bedroom.transform.position.x, 0, bedroom.transform.position.z), fallTime).SetEase(Ease.OutQuint);
+                    }
+                }
+            }
+        }
+    }
 }
