0845a31c77b430ca963e900495951bfe740bed32
Ricky_Huo <113026826+hanbaomo@users.noreply.github.com>
2023-08-04 20:37:47 +1000

    Comments for Beroomzone & storefrontzoneScript

--- Assets/BedRoomZone.cs
+++ Assets/BedRoomZone.cs
@@ -32,23 +32,30 @@ public class BedRoomZone : MonoBehaviour
             {
                 //Storefront will be falling, so set to false.
                 isGoingUp = false;
-                Debug.Log("isGoingUp set to false");
-
+                // Assign the transform component of the game object to a variable, this allows us to access and manipulate the - 
+                // - position, rotation, and scale.
                 Transform storetransform = storefront.transform;
+                // SetEase(Ease.OutQuint) is one type of a animation.
+                // Use.DOMove() to choose a position to move to, and choose the speed time to move to that position.
                 storetransform.DOMove(storetransform.position - new Vector3(0, fallDistance, 0), fallTime).SetEase(Ease.OutQuint);
             }
-
+            //When touches the collider and the y for storeroomposition is -100, move the object up.
             else if(storeroomposition.y == -100)
             {
                 isGoingUp = true;
-                Debug.Log("isGoingUp set to true");
+
                 Transform storetransform = storefront.transform;
                 storetransform.DOMove(storetransform.position + new Vector3(0, fallDistance, 0), fallTime).SetEase(Ease.OutQuint);
             }
+            // This is one of the important code when playing the game.
+            // If the object is moving between 0 - -100, the player touches again, it will have bug.
+            // We need to stop the animation, then make new animation.
             else if (storeroomposition.y < 0 && storeroomposition.y > -100 && isGoingUp == false)
             {
                 isGoingUp = true;
+                //Stop the animation.
                 DOTween.Kill(storefront.transform);
+                // New move, not + or -, using direct position to move to.
                 storefront.transform.DOMove(new Vector3(storefront.transform.position.x, 0, storefront.transform.position.z), fallTime).SetEase(Ease.OutQuint);
             }
             else if (storeroomposition.y < 0 && storeroomposition.y > -100 && isGoingUp == true)
@@ -59,8 +66,10 @@ public class BedRoomZone : MonoBehaviour
             }
         }
     }
+    // After leave the collider it will trigger.
     private void OnTriggerExit(Collider other)
     {
+        // Check this is not null.
         if (playerTransformAnchor != null && playerTransformAnchor.Value != null)
         {
             if (playerTransformAnchor.Value.position.x < -14.45f)
--- Assets/StoreRoomZone.cs
+++ Assets/StoreRoomZone.cs
@@ -2,29 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Player;
-using DG.Tweening;
+using DG.Tweening; //Animation engine for Unity
 
 public class StoreRoomZone : MonoBehaviour
 {
+    // When the player spawn it will assign it to the player, it can get the current player, transform and rotation.
     public TransformAnchor playerTransformAnchor = default;
+    // The object that we want to fall, so this is the storefront zone, we want move the bedroom here.
     public GameObject bedroom;
+    // The speed time when falling.
     public float fallTime;
+    // The distance the storefront falls. "Test": have to test, if the storefront is at this point, our camera should not see storefront anymore.
     public float fallDistance;
+    // We need to know the storefront is moving up or down. up is true, down is false.
     private bool isGoingUp = false;
 
+    //When the object collider touches something.
     private void OnTriggerEnter(Collider other)
     {
+        // When the object collider touches the "Player".
         if (other.CompareTag("Player"))
         {
+            // Get the position of the bedroom.
             Vector3 bedroomposition = bedroom.transform.position;
+            // When the storefront is 0 and the player is lower than the position.
+            // This will not be used because the bedroom always be at the bottom, if the player is in the storefront - 
+            // - and not touching the collider yet.
             if (bedroomposition.y == 0 && playerTransformAnchor.Value.position.x > -11.40f)
             {
+                //Bedroom will be falling, so set to false.
                 isGoingUp = false;
-                Debug.Log("isGoingUp set to false");
+                // Assign the transform component of the game object to a variable, this allows us to access and manipulate the - 
+                // - position, rotation, and scale.
                 Transform storetransform = bedroom.transform;
+                // SetEase(Ease.OutQuint) is one type of a animation.
+                // Use.DOMove() to choose a position to move to, and choose the speed time to move to that position.
                 storetransform.DOMove(storetransform.position - new Vector3(0, fallDistance, 0), fallTime).SetEase(Ease.OutQuint);
             }
-
+            //When touches the collider and the y for bedroomposition is -100, move the object up.
             else if (bedroomposition.y == -100)
             {
                 isGoingUp = true;
@@ -32,10 +47,15 @@ public class StoreRoomZone : MonoBehaviour
                 Transform storetransform = bedroom.transform;
                 storetransform.DOMove(storetransform.position + new Vector3(0, fallDistance, 0), fallTime).SetEase(Ease.OutQuint);
             }
+            // This is one of the important code when playing the game.
+            // If the object is moving between 0 - -100, the player touches again, it will have bug.
+            // We need to stop the animation, then make new animation.
             else if (bedroomposition.y < 0 && bedroomposition.y > -100 && isGoingUp == false)
             {
                 isGoingUp = true;
+                //Stop the animation.
                 DOTween.Kill(bedroom.transform);
+                // New move, not + or -, using direct position to move to.
                 bedroom.transform.DOMove(new Vector3(bedroom.transform.position.x, 0, bedroom.transform.position.z), fallTime).SetEase(Ease.OutQuint);
             }
             else if (bedroomposition.y < 0 && bedroomposition.y > -100 && isGoingUp == true)
@@ -46,8 +66,10 @@ public class StoreRoomZone : MonoBehaviour
             }
         }
     }
+    // After leave the collider it will trigger.
     private void OnTriggerExit(Collider other)
     {
+        // Check this is not null.
         if (playerTransformAnchor != null && playerTransformAnchor.Value != null)
         {
             if (playerTransformAnchor.Value.position.x > -11.40f)
