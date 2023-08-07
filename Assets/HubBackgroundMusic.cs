68ca44ec11afae7368f12139236bd8ca02d5f712
Ricky_Huo <113026826+hanbaomo@users.noreply.github.com>
2023-08-07 22:02:04 +1000

    HUBmusicupdate,error

--- Assets/HubBackgroundMusic.cs
+++ Assets/HubBackgroundMusic.cs
@@ -6,73 +6,69 @@ using Player;
 public class HubBackgroundMusic : MonoBehaviour
 {
     private AudioSource hubbackgroundmusic;
-    // When the player spawn it will assign it to the player, it can get the current player, transform and rotation.
     public TransformAnchor playerTransformAnchor = default;
-
     public GameObject storefront;
-    // Start is called before the first frame update
-    void Start()
+
+    private enum PlayerState
     {
-        hubbackgroundmusic = GetComponent<AudioSource>(); // This will automaticlly get the AudioSource from "this" object.
-        hubbackgroundmusic.enabled = false; // Turn the background music off at the start of the scene.
+        Far,
+        Near
     }
 
-    // Update is called once per frame
+    private PlayerState playerState = PlayerState.Far;
+    private Coroutine volumeCoroutine;
+
+    void Start()
+    {
+        hubbackgroundmusic = GetComponent<AudioSource>();
+        hubbackgroundmusic.enabled = false;
+        hubbackgroundmusic.volume = 0;
+    }
+
     void Update()
     {
-        // Check this is not null.
-        if (playerTransformAnchor != null && playerTransformAnchor.Value != null)
+        Bounds bounds = new Bounds(storefront.transform.position, Vector3.zero);
+        foreach (Transform child in storefront.transform)
         {
-        //{
-        //    float distance = Vector3.Distance(playerTransformAnchor.Value.position, storefront.transform.position);
-        //    if (distance < 3)
-        //    {
-        //        if (!hubbackgroundmusic.enabled)
-        //        {
-        //            hubbackgroundmusic.enabled = true;
-        //        }
-
-        //    }
-        //    if (hubbackgroundmusic.enabled && distance > 5)
-        //    {
-        //        float volume = Mathf.Clamp(1 - ((distance - 3) / 2), 0.2f, 1);
-        //        hubbackgroundmusic.volume = volume;
-        //    }
-        //}
-            Transform parentTransform = storefront.transform;
-
-            bool isWithinDistance = false;
-
-            for (int i = 0; i < parentTransform.childCount; i++)
-            {
-                Transform childTransform = parentTransform.GetChild(i);
-                float distance = Vector3.Distance(playerTransformAnchor.Value.position, childTransform.position);
-
-                if (distance < 3)
-                {
-                    isWithinDistance = true;
-                    break;
-                }
-            }
-
-            if (isWithinDistance)
-            {
-                if (!hubbackgroundmusic.enabled)
-                {
-                    hubbackgroundmusic.enabled = true;
-                }
-            }
-            else if (hubbackgroundmusic.enabled)
-            {
-                float distanceToStorefront = Vector3.Distance(playerTransformAnchor.Value.position, storefront.transform.position);
-                float volume = Mathf.Clamp(1 - ((distanceToStorefront - 3) / 2), 0.2f, 1);
-                hubbackgroundmusic.volume = volume;
-
-                if (distanceToStorefront > 5)
-                {
-                    hubbackgroundmusic.enabled = false; // You may want to disable the music if the distance is too great
-                }
-            }
+            bounds.Encapsulate(child.GetComponent<Renderer>().bounds);
         }
+        float distance = Vector3.Distance(bounds.ClosestPoint(playerTransformAnchor.Value.position), playerTransformAnchor.Value.position);
+
+        if (distance <= 3 && playerState == PlayerState.Far)
+        {
+            playerState = PlayerState.Near;
+            ChangeVolume(0.8f, 8.0f);
+        }
+        else if (distance > 3 && playerState == PlayerState.Near)
+        {
+            playerState = PlayerState.Far;
+            ChangeVolume(0.2f, 3.0f);
+        }
+    }
+
+    private void ChangeVolume(float targetVolume, float duration)
+    {
+        if (volumeCoroutine != null)
+        {
+            StopCoroutine(volumeCoroutine);
+        }
+        volumeCoroutine = StartCoroutine(AdjustVolume(targetVolume, duration));
+    }
+
+    private IEnumerator AdjustVolume(float targetVolume, float duration)
+    {
+        float startTime = Time.time;
+        float startVolume = hubbackgroundmusic.volume;
+
+        hubbackgroundmusic.enabled = true;
+
+        while (Time.time < startTime + duration)
+        {
+            float t = (Time.time - startTime) / duration;
+            hubbackgroundmusic.volume = Mathf.Lerp(startVolume, targetVolume, t);
+            yield return null;
+        }
+
+        hubbackgroundmusic.volume = targetVolume;
     }
 }
