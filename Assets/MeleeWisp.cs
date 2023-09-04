f07c5ed506e2018be72fd963a170c1885e7d786b
Ricky_Huo <113026826+hanbaomo@users.noreply.github.com>
2023-09-05 00:11:20 +1000

    MeleeWisp

---
+++ Assets/MeleeWisp.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class MeleeWisp : MonoBehaviour
+{
+    // Player
+    public TransformAnchor playerTransformAnchor = default;
+    // When to trigger the attack
+    public float interactionDistance = 0.5f;
+    // The Empty object not the real sword, because we need this to rotate the sword.
+    public GameObject Sword;
+    // This is the real object
+    public GameObject RealSword;
+    // Get the rotation, and set a rotation.
+    private Quaternion startRotation;
+    private Quaternion endRotation;
+    // Slash speed
+    public float slashDuration = 0.5f;
+    // Attack Cooldown
+    private float attackCooldown = 1.5f;
+    // Check the next attack time
+    private float nextAttackTime = 0f;
+    // Damage
+    public int damage = 20;
+    // Check slah
+    public bool isslashing = false;
+
+    void Start()
+    {
+        // Get rotation
+        startRotation = Sword.transform.localRotation;
+        // Set rotation
+        endRotation = startRotation * Quaternion.Euler(90, 0, 0);
+    }
+
+    void Update()
+    {
+        // Check the player is spawn or not
+        if (playerTransformAnchor != null && playerTransformAnchor.Value != null)
+        {
+            // If in the trigger distance and the avaliable to do the next attack with 1.5 second.
+            if (Vector3.Distance(transform.position, playerTransformAnchor.Value.position) <= interactionDistance && Time.time >= nextAttackTime)
+            {
+                // Do attack
+                StartCoroutine(SlashRoutine());
+                // Add time for checking next attack
+                nextAttackTime = Time.time + attackCooldown;
+            }
+        }
+    }
+
+    IEnumerator SlashRoutine()
+    {
+        // Create a timer
+        float timeatt = 0;
+        // Smaller than the slashDuration
+        while (timeatt < slashDuration)
+        {
+            isslashing = true;
+            // Each Time in Time.deltaTime will be added in timeatt, the while loop is the whole animation of this Quanternion.Lerp
+            Sword.transform.localRotation = Quaternion.Lerp(startRotation, endRotation, timeatt / slashDuration);
+            timeatt += Time.deltaTime;
+            yield return null;
+        }
+
+        Sword.transform.localRotation = endRotation;
+
+        timeatt = 0;
+        while (timeatt < slashDuration)
+        {
+            isslashing = false;
+            Sword.transform.localRotation = Quaternion.Lerp(endRotation, startRotation, timeatt / slashDuration);
+            timeatt += Time.deltaTime;
+            yield return null;
+        }
+
+        Sword.transform.localRotation = startRotation;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isslashing == true)
+        {
+            PlayerCharacter playerCharacter = playerTransformAnchor.Value.GetComponent<PlayerCharacter>();
+            playerCharacter.TakeDamage(damage);
+        }
+    }
+}
